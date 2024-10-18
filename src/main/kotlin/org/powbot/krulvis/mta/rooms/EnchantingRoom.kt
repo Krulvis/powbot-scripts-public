799598b020f9c4a344e1577c1dbf556774a070f1
htmlfive <shawn.khaan@gmail.com>
2024-10-17 20:21:48 -0400

    Update EnchantingRoom.kt
    Added dragonstone toggle

--- src/main/kotlin/org/powbot/krulvis/mta/rooms/EnchantingRoom.kt
+++ src/main/kotlin/org/powbot/krulvis/mta/rooms/EnchantingRoom.kt
@@ -1,51 +1,70 @@
-package org.powbot.krulvis.mta.rooms
+package org.powbot.krulvis.mta.tree.branches
 
-import org.powbot.api.rt4.Components
-import org.powbot.api.rt4.Inventory
-import org.powbot.api.rt4.Magic
-import org.powbot.api.rt4.Objects
+import org.powbot.api.rt4.*
+import org.powbot.api.script.tree.Branch
+import org.powbot.api.script.tree.SimpleLeaf
 import org.powbot.api.script.tree.TreeComponent
+import org.powbot.krulvis.api.ATContext.walkAndInteract
+import org.powbot.krulvis.api.extensions.Utils.sleep
+import org.powbot.krulvis.api.extensions.Utils.waitFor
+import org.powbot.krulvis.api.extensions.Utils.waitForDistance
 import org.powbot.krulvis.mta.MTA
-import org.powbot.krulvis.mta.tree.branches.CanCastEnchant
+import org.powbot.krulvis.mta.rooms.EnchantingRoom
+import org.powbot.krulvis.mta.tree.leafs.DepositOrbs
+import org.powbot.krulvis.mta.tree.leafs.PickupShape
 
-object EnchantingRoom : MTARoom {
-	override val WIDGET_ID = 195
-	override val portalName: String = "Enchanters"
+class CanCastEnchant(script: MTA) : Branch<MTA>(script, "Can Cast Enchant?") {
+	override val failedComponent: TreeComponent<MTA> = ShouldDepositOrbs(script)
+	override val successComponent: TreeComponent<MTA> = SimpleLeaf(script, "Casting Enchant") {
+		val spell = EnchantingRoom.getEnchantSpell() ?: return@SimpleLeaf
+		if (Magic.magicspell() != spell) {
+			if (spell.cast()) {
+				waitFor { Magic.magicspell() == spell && Game.tab() == Game.Tab.INVENTORY }
+			}
+		}
 
-	override fun rootComponent(mta: MTA): TreeComponent<MTA> = CanCastEnchant(mta)
+		sleep(150)
+		val slot = alchable.inventoryIndex
 
-	const val SHAPE_INDEX_START = 10
-	private val spells = listOf(
-		Magic.Spell.ENCHANT_LEVEL_7_JEWELLERY,
-		Magic.Spell.ENCHANT_LEVEL_6_JEWELLERY,
-		Magic.Spell.ENCHANT_LEVEL_5_JEWELLERY,
-		Magic.Spell.ENCHANT_LEVEL_4_JEWELLERY,
-		Magic.Spell.ENCHANT_LEVEL_3_JEWELLERY,
-		Magic.Spell.ENCHANT_LEVEL_2_JEWELLERY,
-		Magic.Spell.ENCHANT_LEVEL_1_JEWELLERY
-	)
-
-	fun getBonusShape(): Shape {
-		val visibleComp = Components.stream(WIDGET_ID).first { it.index() >= SHAPE_INDEX_START && it.visible() }
-		return Shape.values()[visibleComp.index() - SHAPE_INDEX_START]
+		if (alchable.click()) {
+			waitFor { Inventory.itemAt(slot).name() != alchable.name() }
+		}
 	}
 
-	fun getEnchantable() = Inventory.stream().name(getBonusShape().name, "Dragonstone").first()
-
-	fun getBonusPile() = Objects.stream().nameContains(getBonusShape().name).action("Take-from").nearest().first()
-
-	fun getEnchantSpell(): Magic.MagicSpell? = spells.firstOrNull { it.canCast() }
-
-	fun getDroppables() =
-		Inventory.stream().name(*(Shape.values().toList() - getBonusShape()).map { it.name }.toTypedArray()).toList()
-
-	enum class Shape {
-		Cube,
-		Cylinder,
-		None,
-		Pentamid,
-		None2,
-		Icosahedron,
+	var alchable: Item = Item.Nil
+	override fun validate(): Boolean {
+		val bonusShape = EnchantingRoom.getBonusShape().toString()
+		alchable = Inventory.stream().name(bonusShape, "Dragonstone").first()
+		return alchable.valid()
 	}
-
 }
+
+
+class ShouldDepositOrbs(script: MTA) : Branch<MTA>(script, "Should deposit orbs") {
+	override val failedComponent: TreeComponent<MTA> = ShouldPickupDragonstone(script)
+	override val successComponent: TreeComponent<MTA> = DepositOrbs(script)
+
+	override fun validate(): Boolean {
+		return Inventory.isFull()
+	}
+}
+
+class ShouldPickupDragonstone(script: MTA) : Branch<MTA>(script, "E") {
+	override val failedComponent: TreeComponent<MTA> = PickupShape(script)
+	override val successComponent: TreeComponent<MTA> = SimpleLeaf(script, "Pickup Dragonstone") {
+		if (script.pickupDragonstone && walkAndInteract(ds, "Take")) {
+			waitForDistance(ds) { getDragonStone() != ds }
+		}
+	}
+
+	var ds: GroundItem = GroundItem.Nil
+	
+	private fun getDragonStone() = GroundItems.stream().name("Dragonstone").nearest().first()
+
+	override fun validate(): Boolean {
+		ds = getDragonStone()
+		return ds.valid() && script.pickupDragonstone
+	}
+}
+
+
